db30bfacd66e9db704d434691caddfc169460934
Antonio Bennett <bennettantonio199@gmail.com>
2022-03-11 16:54:10 -0500

    basic structure and modifying output of tests to be object with properties

--- src/tests/test2.ts
+++ src/tests/test2.ts
@@ -35,97 +35,103 @@ const sumOfDigits = (num: number) => {
     return sum;
 };
 
-export const weightedSort = (input = '') => {
-    //If input is invalid type
-    if (typeof input != 'string')
-        return `Invalid Input Type. Expected: "string", Received: "${typeof input}"`;
+export const weightedSort = {
+    title: 'Weighted Sort',
+    desc: 'Enter a space separated list of numbers. This function sorts them by weight which is the sum of the digits of a number',
+    func: (input = '') => {
+        //If input is invalid type
+        if (typeof input != 'string')
+            return `Invalid Input Type. Expected: "string", Received: "${typeof input}"`;
 
-    //Empty string
-    if (input.length < 1) return input;
+        //Empty string
+        if (input.length < 1) return input;
 
-    //Here we do a try catch because instead of going through each num once to check if they are numbers
-    //then going through the list to map each num again and then sorting, we can check and map in one pass
-    //and then sort improving the effeciency
-    let weightedSum: [string, number][];
-    try {
-        weightedSum = input.split(' ').map((num) => {
-            const parsed = filterInt(num);
-            if (isNaN(parsed)) throw new Error(`"${num}" is not a number`);
-            //We return a "tuple" really we are just going to use destructuring assignment later for sorting
-            //We return the string version  of num and it's sum so that if the sum is equal we compare the string for sorting
-            return [num, sumOfDigits(parsed)];
-        });
-    } catch (error) {
-        //Return error message we threw
-        return error.message;
-    }
+        //Here we do a try catch because instead of going through each num once to check if they are numbers
+        //then going through the list to map each num again and then sorting, we can check and map in one pass
+        //and then sort improving the effeciency
+        let weightedSum: [string, number][];
+        try {
+            weightedSum = input.split(' ').map((num) => {
+                const parsed = filterInt(num);
+                if (isNaN(parsed)) throw new Error(`"${num}" is not a number`);
+                //We return a "tuple" really we are just going to use destructuring assignment later for sorting
+                //We return the string version  of num and it's sum so that if the sum is equal we compare the string for sorting
+                return [num, sumOfDigits(parsed)];
+            });
+        } catch (error) {
+            //Return error message we threw
+            return error.message;
+        }
 
-    //This checks if the string value of tuple is equal if so compare them as strings and return otherwise
-    //do regular sorting of numbers the map over sorted array to exclude the sumOfDigits then join by space and return
-    return weightedSum
-        .sort((a, b) => {
-            const [aString, aSum] = a;
-            const [bString, bSum] = b;
+        //This checks if the string value of tuple is equal if so compare them as strings and return otherwise
+        //do regular sorting of numbers the map over sorted array to exclude the sumOfDigits then join by space and return
+        return weightedSum
+            .sort((a, b) => {
+                const [aString, aSum] = a;
+                const [bString, bSum] = b;
 
-            if (aSum === bSum)
-                return aString < bString ? Number(-1) : Number(1);
+                if (aSum === bSum)
+                    return aString < bString ? Number(-1) : Number(1);
 
-            return aSum - bSum;
-        })
-        .map(([num, _]) => num)
-        .join(' ');
+                return aSum - bSum;
+            })
+            .map(([num, _]) => num)
+            .join(' ');
+    },
 };
 
 if (import.meta.vitest) {
     const { it, expect } = import.meta.vitest;
     it('returns invalid input types', () => {
         //Numbers
-        expect(weightedSort(1)).toBe(
+        expect(weightedSort.func(1)).toBe(
             'Invalid Input Type. Expected: "string", Received: "number"',
         );
-        expect(weightedSort(1.65)).toBe(
+        expect(weightedSort.func(1.65)).toBe(
             'Invalid Input Type. Expected: "string", Received: "number"',
         );
-        expect(weightedSort(NaN)).toBe(
+        expect(weightedSort.func(NaN)).toBe(
             'Invalid Input Type. Expected: "string", Received: "number"',
         );
         //Boolean
-        expect(weightedSort(true)).toBe(
+        expect(weightedSort.func(true)).toBe(
             'Invalid Input Type. Expected: "string", Received: "boolean"',
         );
         //Undefined - This test returns '' because we give a default value of '' when input argument is undefined
-        expect(weightedSort(undefined)).toBe('');
+        expect(weightedSort.func(undefined)).toBe('');
         //Arrays/Objects <- Arrays are objects
-        expect(weightedSort({ test: 'test' })).toBe(
+        expect(weightedSort.func({ test: 'test' })).toBe(
             'Invalid Input Type. Expected: "string", Received: "object"',
         );
-        expect(weightedSort([1, 2, 3])).toBe(
+        expect(weightedSort.func([1, 2, 3])).toBe(
             'Invalid Input Type. Expected: "string", Received: "object"',
         );
-        expect(weightedSort(() => {})).toBe(
+        expect(weightedSort.func(() => {})).toBe(
             'Invalid Input Type. Expected: "string", Received: "function"',
         );
     });
     it('returns known invalid string', () => {
         //Check empty strings and lists with non numbers in them
-        expect(weightedSort()).toBe('');
-        expect(weightedSort('')).toBe('');
-        expect(weightedSort('t')).toBe('"t" is not a number');
-        expect(weightedSort('8 16 124 x 7 91')).toBe('"x" is not a number');
-        expect(weightedSort('8,16,124,12,7,91')).toBe(
+        expect(weightedSort.func()).toBe('');
+        expect(weightedSort.func('')).toBe('');
+        expect(weightedSort.func('t')).toBe('"t" is not a number');
+        expect(weightedSort.func('8 16 124 x 7 91')).toBe(
+            '"x" is not a number',
+        );
+        expect(weightedSort.func('8,16,124,12,7,91')).toBe(
             '"8,16,124,12,7,91" is not a number',
         );
-        expect(weightedSort('8, 16, 124, 12, 7, 91')).toBe(
+        expect(weightedSort.func('8, 16, 124, 12, 7, 91')).toBe(
             '"8," is not a number',
         );
     });
     it('sorts list by weight', () => {
         //Check variety of lists
-        expect(weightedSort('56 65 74 100 99 68 86 180 90')).toBe(
+        expect(weightedSort.func('56 65 74 100 99 68 86 180 90')).toBe(
             '100 180 90 56 65 74 68 86 99',
         );
-        expect(weightedSort('1 17 9 4 20')).toBe('1 20 4 17 9');
-        expect(weightedSort('1')).toBe('1');
-        expect(weightedSort('8 26')).toBe('26 8');
+        expect(weightedSort.func('1 17 9 4 20')).toBe('1 20 4 17 9');
+        expect(weightedSort.func('1')).toBe('1');
+        expect(weightedSort.func('8 26')).toBe('26 8');
     });
 }
